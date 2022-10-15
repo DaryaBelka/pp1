ce6cdad16663c1b4cb5273713f4bddcdbb54d69e
Darya Belka <225284@student.uek.krakow.pl>
2022-10-15 19:42:21 +0200

    zadanie

---
+++ 02-TypesAndVariables/homework/ch3.zad2.py
@@ -0,0 +1,8 @@
+a = int(input("Podaj liczbę godzin:"))
+b = int(input("Podaj stawkę godzinową:"))
+
+else:
+    if a > 40 :
+     print(f'Wynagrodzenie: {40*b+(a-40)*b*1.5}')
+    else :
+     print(f'Wynagrodzenie: {a*b}')
