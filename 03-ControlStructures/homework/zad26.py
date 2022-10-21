46ae78f565096a30ee4a5ddc92b5dba00ab64d6a
Darya Belka <225284@student.uek.krakow.pl>
2022-10-21 20:51:18 +0200

    zadanie

--- 03-ControlStructures/homework/zad26.py
+++ 03-ControlStructures/homework/zad26.py
@@ -1,1 +1,10 @@
-
+for i in range(1,4):
+    a=input("Enter the PIN code: ")
+    if a=="0805":
+        print('Your PIN code is correct', end = '')
+    else:
+        if i<3:
+            print("Incorrect...")
+        if i==3:
+            print("Sorry,your payment card has been blocked")
+    
