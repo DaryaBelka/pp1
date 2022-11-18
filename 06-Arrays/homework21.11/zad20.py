f0f4fa770e4e2c9c3bc71e0ec04e0c15899b0caa
Darya Belka <225284@student.uek.krakow.pl>
2022-11-19 00:54:53 +0100

    zad

--- 06-Arrays/homework21.11/zad20.py
+++ 06-Arrays/homework21.11/zad20.py
@@ -1,12 +1,10 @@
 arr = [12,6,4,9,10]
 def star(n):
    for n in arr:
-      if n==12 or n==6 or n==4 or n==9 or n==10:
-         print(n: '*')
-      else: 
-         pass
+         if dd
+         else: 
+            pass
    return 
-result = star(arr)
-print(result)
+print(star())
 
 
---
+++ 06-Arrays/homework21.11/zad25.py
@@ -0,0 +1,7 @@
+def occurs(number, array):
+   for i in range(len(array)):
+      if i==number:
+         print('number {number} appears in the array')
+      else:
+         print('number {number} dont appears in the array')
+print('23, ([15,38,7,23,14])')
