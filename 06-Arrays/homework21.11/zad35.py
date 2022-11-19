f09787e6c00c5d8702b30ec60f610ec757979a0f
Darya Belka <225284@student.uek.krakow.pl>
2022-11-19 05:26:42 +0100

    zad

---
+++ 06-Arrays/homework21.11/zad35.py
@@ -0,0 +1,7 @@
+import random
+def rand_elem(array):
+   a = random.choice(array)
+   return a
+print(rand_elem([2,4,6,8])) #fewrandomliarray
+print(rand_elem([1,3,5,7,9]))
+   
---
+++ 06-Arrays/homework21.11/zad36.py
@@ -0,0 +1,5 @@
+array = [[1, 2, 5, 6], [7, 8, 3, 8]]
+for i in range(len(array)):
+   for j in range(len(array[i])):
+        print(array[i][j], end=' ')
+   print()
