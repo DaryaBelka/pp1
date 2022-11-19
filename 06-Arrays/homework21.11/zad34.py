5a25291336ec7245a803898b70318b4410bf20d2
Darya Belka <225284@student.uek.krakow.pl>
2022-11-19 05:09:29 +0100

    zad

---
+++ 06-Arrays/homework21.11/zad34.py
@@ -0,0 +1,8 @@
+arr1 = input('enter arr1: ')
+arr2 = input('enter arr2: ')
+for i in range(len(arr2)):
+   if arr2[i] in arr1:
+      print('The first array is a subset of the second one')
+   else:
+      print('The first array is a subset of the second one')
+
