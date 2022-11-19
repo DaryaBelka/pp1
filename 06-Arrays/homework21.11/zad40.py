c18409140b26fccf9f75c5d36e1239345c9c1c30
Darya Belka <225284@student.uek.krakow.pl>
2022-11-19 17:15:02 +0100

    zad

--- 06-Arrays/homework21.11/zad40.py
+++ 06-Arrays/homework21.11/zad40.py
@@ -1,5 +1,15 @@
-array = [[-38,19],[5,40],[-7,11],[29,16]]
-smallest = min(array)
-largest = max(array)
-result = largest - smallest
-print(f'{result}')
+array = [[-38, 19], [5, 40], [-7, 11], [29, 16]]
+minimum = 0
+maximum = 0
+for i in range(len(array)):
+    for j in range(len(array[i])):
+        if array[i][j] < minimum:
+            minimum = array[i][j]
+            indminr = i
+            indminc = j
+        elif array[i][j] > maximum:
+            maximum = array[i][j]
+            indmaxr = i
+            indmaxc = j
+print("Smallest value: ", minimum, '(row:', indminr, ',', 'column: ', indminc, ')')
+print("Largest value: ", maximum, '(row: ', indmaxr, ',', 'column: ', indmaxc, ')')
