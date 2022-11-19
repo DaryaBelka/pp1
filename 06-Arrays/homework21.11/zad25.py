08f99149ebf765dc9b716f3c93ed209f6da3be1b
Darya Belka <225284@student.uek.krakow.pl>
2022-11-19 01:25:03 +0100

    zad

--- 06-Arrays/homework21.11/zad25.py
+++ 06-Arrays/homework21.11/zad25.py
@@ -1,7 +1,9 @@
+number = input('enter number: ')
+array = input('enter array: ')
 def occurs(number, array):
    for i in range(len(array)):
       if i==number:
          print('number {number} appears in the array')
       else:
          print('number {number} dont appears in the array')
-print('23, ([15,38,7,23,14])')
+result = occurs(number, array)
