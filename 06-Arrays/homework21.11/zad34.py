3f9a211b113df40cb193edee3f347c9cd191bd51
Darya Belka <225284@student.uek.krakow.pl>
2022-11-20 21:36:04 +0100

    zad

--- 06-Arrays/homework21.11/zad34.py
+++ 06-Arrays/homework21.11/zad34.py
@@ -1,8 +1,15 @@
-arr1 = input('enter arr1: ')
-arr2 = input('enter arr2: ')
-for i in range(len(arr2)):
-   if arr2[i] in arr1:
+arr1 = [9,4,5,8,10]
+arr2 = [1,2,3]
+
+print('Orignal list:' + str(arr1))
+print('Orignal sub list:' + str(arr2))
+
+flag = 0 
+if(all(x in arr1 for x in arr2)):
+   flag = 1
+
+if (flag):
       print('The first array is a subset of the second one')
-   else:
-      print('The first array is a subset of the second one')
+else:
+      print('The first array is not a subset of the second one')
 
