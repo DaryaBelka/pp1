b6ed535278e8a41a242c092d6e32ee657d7834a6
Darya Belka <225284@student.uek.krakow.pl>
2022-11-21 21:14:30 +0100

    zad

---
+++ 07-FileHandling/zad20.py
@@ -0,0 +1,9 @@
+import random
+a=random.randint(100,999)
+f=open('.\\07-FileHandling\\zad20','w')
+for i in range(50):
+    f.write(f'{random.randint(100,999)}\n')
+f.close
+f=open('.\\07-FileHandling\\zad20.txt','r')
+print(f.read())
+f.close()
