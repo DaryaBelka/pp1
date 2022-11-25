06fcf9277e36db6a66d7f9d6a46fc731730998ca
Darya Belka <225284@student.uek.krakow.pl>
2022-11-25 22:56:51 +0100

    zad

--- 07-FileHandling/zad15.py
+++ 07-FileHandling/zad15.py
@@ -1,7 +1,4 @@
 file = open('.\\07-FileHandling\\zad15.txt','r') 
-file_content = file.read() 
-licznik = 0 
-for line in file:
-   print(f'{licznik}')
-print( file_content ) 
+print(file.readlines()[0])
+
 file.close()
