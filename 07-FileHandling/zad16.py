71399827be6d7a0c49e87b42391bf974ed2c0b99
Darya Belka <225284@student.uek.krakow.pl>
2022-11-25 23:35:09 +0100

    zad

--- 07-FileHandling/zad16.py
+++ 07-FileHandling/zad16.py
@@ -1,8 +1,6 @@
-import shutil
+file=open('.\\07-FileHandling\\zad15.txt','r')
+a = file.read()
+file.close()
 file=open('.\\07-FileHandling\\copy.txt','w')
-a = shutil.copy('\\07-FileHandling\\zad15.txt', 'r')
-file.write(a)
-file.close
-file=open('.\\07-FileHandling\\copy.txt','r')
-print(file.read())
+b = file.write(f'{a}')
 file.close()
