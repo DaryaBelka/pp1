4536f02aa4481fd94d72579df2f9266365e618d6
Darya Belka <225284@student.uek.krakow.pl>
2022-11-27 20:43:39 +0100

    zad

--- 09-Test2/p3.py
+++ 09-Test2/p3.py
@@ -1,2 +1,15 @@
 def f(array2D):
-   
+   sum = 0
+   sum1 = 0
+   sum2 = 0
+   sum3 = 0
+   for i in range(len(array2D)):
+      sum += array2D[i][0]
+      sum1 += array2D[i][1]
+      sum2 += array2D[i][2]
+      sum3 += array2D[i][-1]
+   return sum, sum1, sum2, sum3
+array2D = [3,6,2,7], [9,5,4,0], [2,8,0,9]
+print(f(array2D))
+
+
