cef54b21db636eaf5e11346bdf8fa0a25933a6bd
Darya Belka <225284@student.uek.krakow.pl>
2022-12-03 19:27:41 +0100

    zad

--- 09-Test2/p5.py
+++ 09-Test2/p5.py
@@ -1,10 +1,11 @@
 def f(first_letter,last_letter):
-   licznik = 0
+  
    with open("test.txt", "r") as file:
+      licznik = 0
       for line in file:
          words_in_line = line.split()
          for word in words_in_line:
             if first_letter == word[0] and last_letter == word[-1]:
                licznik += 1
-   return licznik
+      return licznik
 print(f("w","d"))
--- 09-Test2/p6.py
+++ 09-Test2/p6.py
@@ -1,3 +1,11 @@
+import json
 def f(age, course, average):
    with open("test.json", "r") as file:
-      
+      licznik = 0
+      for line in file:
+         students_line = line.split()
+         for student in students_line:
+            if age in student and course in student and average in student:
+               licznik += 1
+      return licznik
+print(f(21, "statistics", 4))
