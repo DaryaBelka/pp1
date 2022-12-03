14a8225dbea6c00334169dd80d74c08a9e1cb047
Darya Belka <225284@student.uek.krakow.pl>
2022-12-03 16:59:19 +0100

    zad

--- 09-Test2/p5.py
+++ 09-Test2/p5.py
@@ -3,7 +3,7 @@ def f(first_letter,last_letter):
    with open("test.txt", "r") as file:
       for i in file:
          for j in i:
-            if first_letter == j[0] and last_letter[-1]:
+            if first_letter == j[0] and last_letter == j[-1]:
                licznik += j
                return licznik
 print(f("w","d"))
