a5ed98826d57095db70b62f7d4abebd0c8215a2b
Darya Belka <225284@student.uek.krakow.pl>
2022-12-03 17:29:30 +0100

    zad

--- 09-Test2/p5.py
+++ 09-Test2/p5.py
@@ -1,11 +1,11 @@
 def f(first_letter,last_letter):
    licznik = 0
-   with open("test.txt", "r") as file:
+   with open("test1.txt", "r") as file:
       for line in file:
          words_in_line = line.split()
          for word in words_in_line:
             if first_letter == word[0] and last_letter == word[-1]:
                licznik += 1
-         return licznik
+   return licznik
 print(f("w","d"))
 print(f("u","d"))
