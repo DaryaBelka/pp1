0c7618ccd12533b004cda0b259486da146ad52ab
Darya Belka <225284@student.uek.krakow.pl>
2022-12-03 17:11:44 +0100

    zad

--- 09-Test2/p5.py
+++ 09-Test2/p5.py
@@ -6,5 +6,6 @@ def f(first_letter,last_letter):
          for word in words_in_line:
             if first_letter == word[0] and last_letter == word[-1]:
                licznik += 1
-            return licznik
+         return licznik
 print(f("w","d"))
+print(f("u","d"))
