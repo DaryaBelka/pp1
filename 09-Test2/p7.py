2ac56b6bb9de7d0330c0f823f977adc88b5206bb
Darya Belka <225284@student.uek.krakow.pl>
2022-12-04 20:30:42 +0100

    zad

--- 09-Test2/p7.py
+++ 09-Test2/p7.py
@@ -1,10 +1,11 @@
 import csv
-def f(gender):
+def f(age, gender):
    count = 0 
    with open("test.csv", "r", encoding="UTF-8") as file:
       data = csv.reader(file)
       for i in data:
-         if i[3] == gender:
+         a = int(i[3])
+         if a >= age and i[2] == gender:
             count += 1
    return count
-print(f("Female"))
+print(f(40,"Female"))
