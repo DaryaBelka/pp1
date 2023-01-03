1ad993f5c3c7f83b4377cb3bb85c00aa4db61380
Darya Belka <225284@student.uek.krakow.pl>
2023-01-03 09:24:26 +0100

    zad

---
+++ 13-Test3/p3.py
@@ -0,0 +1,10 @@
+def f1(t):
+   import re
+   i = re.findall(r'(\w+) (\d+)', t)
+  
+   return 
+def f2(d):
+   total_age = sum(d.values())
+   return total_age
+
+print(f1("Mark is 24 and Ann is 27"))
